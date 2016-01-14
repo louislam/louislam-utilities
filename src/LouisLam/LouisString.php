403f476523cc6238241b7efb20f750c46ff69665
Louis Lam <louislam@live.com>
2016-01-14 11:35:40 +0800

    add startsWith and endsWith

--- src/LouisLam/LouisString.php
+++ src/LouisLam/LouisString.php
@@ -26,4 +26,26 @@ class LouisString
         return mb_substr($str, $index, 1, 'utf-8');
     }
 
+    /**
+     * FROM: http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+     * @param $haystack
+     * @param $needle
+     * @return bool
+     */
+    public static function startsWith($haystack, $needle) {
+        // search backwards starting from haystack length characters from the end
+        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+    }
+
+    /**
+     * FROM: http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+     * @param $haystack
+     * @param $needle
+     * @return bool
+     */
+    public static function endsWith($haystack, $needle) {
+        // search forward starting from end minus needle length characters
+        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+    }
+
 }
